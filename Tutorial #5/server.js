c05a64220e8d1967b3000794f592b66141819560
Dith-ID <70526408+Dith-ID@users.noreply.github.com>
2020-09-10 06:16:11 +0700

    Update server.js

--- Tutorial #5/server.js
+++ Tutorial #5/server.js
@@ -1,19 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-//UPTIME ROBOT (WEB)
-const { get } = require("snekfetch");
-const http = require("http");
-const express = require("express");
-const app = express();
-app.get("/", (request, response) => {
-  console.log("Pinging");
-  response.sendStatus(200);
-})
-app.listen(process.env.PORT);
-setInterval(() => {
-
-http.get('live app http');
-}, 280000);
 
 client.on("ready", async () => {
   console.log(`${client.user.tag} sudah online!`);
